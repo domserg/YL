d749084ea66232f78bd037c096d02d7c310ef76d
domserg <domserg@rambler.ru>
2023-07-14 19:51:50 +0300

    Add files via upload

---
+++ install_lib_yandex.bat
@@ -0,0 +1,3 @@
+python.exe -m pip install -U pip
+pip install -U setuptools pillow pymorphy2 pymorphy2-dicts-ru python-docx docxtpl python-pptx xlsxwriter numpy opencv-python pyinstaller pyinstaller-hooks-contrib
+pip list -o
